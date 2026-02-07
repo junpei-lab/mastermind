3200831d199526ffe065d59ad5650bc0e5c97997
Junpei Yamamoto <baby.no.kidding@gmail.com>
2026-02-07 13:48:00 +0900

    Add GitHub Actions workflow for deploying to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const repository = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
+const isUserOrOrgPagesRepo = repository.endsWith('.github.io');
+const base = process.env.GITHUB_ACTIONS
+  ? isUserOrOrgPagesRepo
+    ? '/'
+    : `/${repository}/`
+  : '/';
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base,
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
